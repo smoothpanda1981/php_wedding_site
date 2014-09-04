c7069a3ed85f2e76d85418be0f9e1f7a05eda146
Yan Wang <smoothpanda1981@gmail.com>
2014-09-04 09:03:28 +0200

    switch db connection

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 ob_start();
 
 session_start(); 
-$db_connect = mysql_connect("localhost", "root", "root");
-//$db_connect = mysql_connect("mysql-yanwangjelastic2.appengine.flow.ch", "root", "IlqNuwZAhJ");
+//$db_connect = mysql_connect("localhost", "root", "root");
+$db_connect = mysql_connect("mysql-yanwangjelastic2.appengine.flow.ch", "root", "IlqNuwZAhJ");
 
 if (!$db_connect) {
     die('Connexion impossible : ' . mysql_error());
